1919250e49d2d5baf5673ffe552641c63d3d0fac
Verite Mugabo <mugaboverite@gmail.com>
2024-11-26 16:57:26 -0500

    feat: add stuffs

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
+this projects assumes you have [nodejs](https://nodejs.org/en) setup on your system. Go a head an run.
+
 ```
 npm install
 npm run dev
 ```
 
 ```
-open http://localhost:3000
+open http://localhost:8000
+```
+
+```sh
+curl -X POST http://localhost:8000/add -H "Content-Type: application/json" -d '{"payer": "DANNON", "points": 300, "timestamp": "2022-10-31T10:00:00Z"}'
+
+curl -X POST http://localhost:8000/add -H "Content-Type: application/json" -d '{"payer": "UNILEVER", "points": 200, "timestamp": "2022-10-31T11:00:00Z"}'
+
+curl -X POST http://localhost:8000/add -H "Content-Type: application/json" -d '{"payer": "DANNON", "points": -200, "timestamp": "2022-10-31T15:00:00Z"}'
+
+curl -X POST http://localhost:8000/add -H "Content-Type: application/json" -d '{"payer": "MILLER COORS", "points": 10000, "timestamp": "2022-11-01T14:00:00Z"}'
+
+curl -X POST http://localhost:8000/add -H "Content-Type: application/json" -d '{"payer": "DANNON", "points": 1000, "timestamp": "2022-11-02T14:00:00Z"}'
+```
+
+```sh
+curl http://localhost:8000/balance
 ```
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,93 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('api is up and running!')
+})
+
+type UserPoints = {
+  payer: string,
+  points: number,
+  timestamp: string
+}
+
+const db: UserPoints[] = []
+
+app.post("/add", async (c) => {
+   const body = await c.req.json<UserPoints>()
+
+   // validation
+   if(!body.payer){
+	  return c.newResponse("payer is required",400)
+   }
+
+   if(!body.points){
+	return c.newResponse("points is a required field",400)
+   }
+
+   if(isNaN(body.points)){
+	return c.newResponse("points field should be a number",400)
+   }
+
+   if(isNaN(Date.parse(body.timestamp))){
+	 return c.newResponse("timestamp field is a required and should be a valid timestamp",400)
+   }
+
+   db.push(body)
+   
+    return c.body(null,200)
+})
+
+type spendRequest = {
+	points: number
+}
+
+app.post("/spend",async(c) => {
+	const body = await c.req.json<spendRequest>()
+
+	if(!body.points || isNaN(body.points)){
+		return c.newResponse("points field is required and should be a number",400)
+	}
+
+	// get total points
+	let sum = db.reduce((prev,cur) => prev + cur.points, 0)
+
+	if (sum < body.points){
+	  return c.newResponse("the user doesn't have enough points.", 400)
+	}
+
+    //@ts-ignore
+	db.sort((a,b) => new Date(a.timestamp) - new Date(b.timestamp))
+
+	const transHist: Record<string, number> = {}
+
+	sum = body.points
+
+	console.log(db)
+
+	for(let i = 0; i < db.length; i++){
+		if(sum == 0) break
+
+		console.log("sum: "+ sum)
+
+		const reduction = Math.min(db[i].points, sum)
+
+		db[i].points -= reduction
+		sum -= reduction
+
+		transHist[db[i].payer] = -reduction
+	}
+
+	return c.json(transHist)
+})
+
+app.get("/balance", async (c) => {
+	const balances: Record<string, number> = {}
+
+	for(const item of db){
+		balances[item.payer] = item.points
+	}
+
+	return c.json(balances, 200)
 })
 
 const port = 8000
